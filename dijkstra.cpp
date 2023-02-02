830a2163adf0749226e881606d2ca9c715e2a9ca
NebojsaJovanovic95 <nebojsajovanovic95@gmail.com>
2023-02-02 11:04:07 -0500

    dijkstra.cpp modified, p_q takes pair <cost, index> vs <index, cost> and we now check if the cost is an improvement before we push the pair to p_q, which saves a lot of computation

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -14,22 +14,20 @@ int distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t
   int result = -1;
   vector<int> dist(adj.size(), -1);
   priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > q;
-  q.push(make_pair(s, 0));
+  q.push(make_pair(0, s));
   while (!q.empty()) {
     pair<int, int> par = q.top();
-    int x = par.first;
-    int d = par.second;
+    int d = par.first;
+    int x = par.second;
+    //std::cout << x << " " << d << "\n";
     q.pop();
-    if (x == t) {
-        result = d;
-        break;
-    }
     for (size_t i = 0; i < adj[x].size(); i ++)
-      if (dist[adj[x][i]] == -1)
-        q.push(make_pair(adj[x][i], d + cost[x][i]));
-    dist[x] = d;
+      if (dist[adj[x][i]] == -1 || d + cost[x][i] < dist[adj[x][i]]) {
+        q.push(make_pair(d + cost[x][i], adj[x][i]));
+        dist[adj[x][i]] = d + cost[x][i];
+      }
   }
-  return result;
+  return dist[t];
 }
 
 int main() {
